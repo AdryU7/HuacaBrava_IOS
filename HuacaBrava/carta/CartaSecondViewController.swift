0301bfd3c4be29ca4cc8336ed18bd2cef0a38df8
adrym1 <i202110771@cibertec.edu.pe>
2023-10-11 12:12:33 -0500

    Se agrego fondo al LaunchScreen y se intentó la función de acercar en una imagen de la carta

--- HuacaBrava/carta/CartaSecondViewController.swift
+++ HuacaBrava/carta/CartaSecondViewController.swift
@@ -18,5 +18,22 @@ class CartaSecondViewController: UIViewController {
         
         fotoCartaImageView.image = imagen
     }
+    
+    @IBAction func pinchImage(_ sender: UIPinchGestureRecognizer) {
+        if sender.state == .began || sender.state == .changed {
+            let currentScale = fotoCartaImageView.frame.size.width / fotoCartaImageView.bounds.size.width
+            var newScale = sender.scale * currentScale
+            
+            // Limita el valor mínimo y máximo de la escala
+            newScale = max(newScale, 1.0)
+            newScale = min(newScale, 3.0)
+            
+            // Aplica la transformación de escala a la imagen
+            let transform = fotoCartaImageView.transform.scaledBy(x: newScale, y: newScale)
+            fotoCartaImageView.transform = transform
 
+            sender.scale = 1.0
+        }
+    }
+    
 }
