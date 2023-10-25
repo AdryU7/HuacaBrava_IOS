984512460a2b7d2ee684a38eda06e668bf522d40
AlonzoJaurergui <jaureguiotoya@gmail.com>
2023-10-25 13:35:40 -0500

    Agregado de contenido inicio

--- HuacaBrava/InicioViewController.swift
+++ HuacaBrava/InicioViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class InicioViewController: UIViewController {
+class InicioViewController: UIViewController, UICollectionViewDataSource {
 
+    @IBOutlet weak var productosCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        productosCollectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "InicioCollectionViewCell", for: indexPath) as! InicioCollectionViewCell
+        cell.productoImagenView.image = UIImage(named: "pollo_a_la_brasa")
+        return cell
+    }
+    
 
 }
---
+++ HuacaBrava/inicio/InicioCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  InicioCollectionViewCell.swift
+//  HuacaBrava
+//
+//  Created by DAMII on 25/10/23.
+//
+
+import UIKit
+
+class InicioCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productoImagenView: UIImageView!
+}
